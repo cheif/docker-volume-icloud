49ae05b0b86c7729e07926576c78361be1c10a61
Dan Berglund <dan@berglund.io>
2023-10-13 10:27:58 +0200

    Add tests for icloud package

---
+++ icloud/icloud_test.go
@@ -0,0 +1,50 @@
+package icloud
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"testing"
+	"time"
+)
+
+func TestListenToChanges(t *testing.T) {
+	accessToken := os.Getenv("ACCESS_TOKEN")
+	if accessToken == "" {
+		log.Fatalf("ACCESS_TOKEN required!")
+	}
+	webauthUser := os.Getenv("WEBAUTH_USER")
+	if webauthUser == "" {
+		log.Fatalf("WEBAUTH_USER required!")
+	}
+	client := http.Client{}
+	client.Jar = AuthenticatedJar(accessToken, webauthUser)
+	drive := Drive{
+		client: client,
+	}
+	err := drive.ValidateToken()
+	if err != nil {
+		t.Error(err)
+	}
+
+	file, err := drive.GetNode("/test/testfile.txt")
+	if err != nil {
+		t.Error(err)
+	}
+	log.Println("Observing /test/testfile.txt, please change it!")
+	initialDateChanged := file.DateChanged
+	log.Println("Initial DateChanged:", initialDateChanged)
+	for true {
+		time.Sleep(time.Second)
+		file, err = drive.GetNode("/test/testfile.txt")
+		if err != nil {
+			t.Error(err)
+		}
+		if initialDateChanged != file.DateChanged {
+			break
+		} else {
+			log.Println("Still", file.DateChanged)
+		}
+	}
+	log.Println("New DateChanged:", file.DateChanged)
+}
